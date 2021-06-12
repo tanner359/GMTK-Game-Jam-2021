45599e3fb64b15816fa9ce69264d9d940f3582f5
Tanner Cobb <tannercobb359@gmail.com>
2021-06-12 03:15:31 -0400

    Fixed customer interaction, seating fixed, and happiness meter

--- HoneyFox_GMTK_Jam/Assets/Scripts/Customer/CustomerBehaviour.cs
+++ HoneyFox_GMTK_Jam/Assets/Scripts/Customer/CustomerBehaviour.cs
@@ -7,6 +7,8 @@ public class CustomerBehaviour : MonoBehaviour
     public Vector3 targetPostion;
     public Seat targetSeat;
     public bool isSeated;
+    public SpriteRenderer happinessGauge;
+    public float happiness = 100;
 
     private void Start()
     {
@@ -25,7 +27,29 @@ public class CustomerBehaviour : MonoBehaviour
                 targetPostion = targetSeat.transform.position;
             }
         }
+        else
+        {
+            if (!isWaiting)
+            {
+                StartCoroutine(HappinessDecay());
+                isWaiting = true;
+            }
+        }
 
         transform.position = Vector3.MoveTowards(transform.position, targetPostion, 0.01f);    
+    }
+
+    bool isWaiting;
+    public IEnumerator HappinessDecay()
+    {
+        if(happiness == 0) { StopAllCoroutines(); targetPostion = BarManager.instance.exit.transform.position; }
+        if(happiness == 100)
+        {
+            yield return new WaitForSeconds(5f);
+        }
+        yield return new WaitForSeconds(1f);
+        happiness--;
+        happinessGauge.color = Color.HSVToRGB(happiness/360.0f, 1.0f, 1.0f);
+        StartCoroutine(HappinessDecay());
     }
 }
