2283210ac2810c2651aebf142c6a478193dacdd4
Devon <devurauxe@gmail.com>
2021-06-13 10:44:51 -0400

    some options work now

--- HoneyFox_GMTK_Jam/Assets/Scripts/OptionsMenu.cs
+++ HoneyFox_GMTK_Jam/Assets/Scripts/OptionsMenu.cs
@@ -1,10 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class OptionsMenu : MonoBehaviour
 {
     public Canvas main_Menu;
+    public AudioSource background_Music;
 
     public void Back_To_Main()
     {
@@ -12,8 +14,13 @@ public class OptionsMenu : MonoBehaviour
         main_Menu.enabled = true;
     }
 
-    public void Fullscreen_Toggle(bool option)
+    public void Fullscreen_Toggle()
     {
-        Screen.fullScreen = option;
+        Screen.fullScreen = GameObject.FindGameObjectWithTag("FS").GetComponent<Toggle>().isOn;
+    }
+
+    public void Music_Toggle()
+    {
+        background_Music.mute = GameObject.FindGameObjectWithTag("Mute").GetComponent<Toggle>().isOn;
     }
 }
